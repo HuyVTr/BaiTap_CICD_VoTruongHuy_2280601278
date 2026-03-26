18f58dc19758fef34df9534f4423d77be99dc398
Huy Võ <votruonghuy25@gmail.com>
2026-03-26 14:22:20 +0700

    first commit

---
+++ VoTruongHuy_2280601278/src/main/java/com/example/VoTruongHuy_2280601278/VoTruongHuy2280601278Application.java
@@ -0,0 +1,13 @@
+package com.example.VoTruongHuy_2280601278;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoTruongHuy2280601278Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoTruongHuy2280601278Application.class, args);
+	}
+
+}
---
+++ VoTruongHuy_2280601278/src/test/java/com/example/VoTruongHuy_2280601278/VoTruongHuy2280601278ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VoTruongHuy_2280601278;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoTruongHuy2280601278ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
